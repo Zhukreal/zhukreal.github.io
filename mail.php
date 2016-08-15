da96d732f5be3f8ec65927d85b0c05749c8fa7cb
Zhuk <Zhuk>
2016-08-16 00:56:55 +0300

    first commit

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+$recepient = "Zhukreal@gmail.com";
+$sitename = "Scontent.by";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+$text = trim($_GET["text"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+
+
